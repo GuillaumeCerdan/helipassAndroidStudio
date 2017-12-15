330a75e5e4ef1d273c28cea4792fb72b27e3c208
Guillaume <gcerdan83@gmail.com>
2017-12-15 17:43:28 +0100

    Unhandled exception

---
+++ app/src/main/java/agency/getup/helipass/Offer.java
@@ -0,0 +1,31 @@
+package agency.getup.helipass;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public class Offer {
+    private String name;
+    private double longitude;
+    private double latitude;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName() {
+        this.name = name;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude() {
+        this.longitude = longitude;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude() {
+        this.latitude = latitude;
+    }
+}
